5edc7bc77fbf84e31049af8b8a45fd8b18c8a4c6
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2021-05-29 14:24:37 +0200

    (Extraccion de metodo) --> 2º commit

--- practica2Uf2V2/src/practica2Uf2V2/Client.java
+++ practica2Uf2V2/src/practica2Uf2V2/Client.java
@@ -46,26 +46,8 @@ public class Client {
             getNom() +
             " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = 0;
-            switch (lloguer.getVehicle().getCategoria()) {
-                case Vehicle.BASIC:
-                    quantitat += 3;
-                    if (lloguer.getDies() > 3) {
-                        quantitat += (lloguer.getDies() - 3) * 1.5;
-                    }
-                    break;
-                case Vehicle.GENERAL:
-                    quantitat += 4;
-                    if (lloguer.getDies() > 2) {
-                        quantitat += (lloguer.getDies() - 2) * 2.5;
-                    }
-                    break;
-                case Vehicle.LUXE:
-                    quantitat += lloguer.getDies() * 6;
-                    break;
-            }
-
-            // afegeix lloguers freqüents
+        	double quantitat = quantitatPerLloguer(lloguer);
+        	// afegeix lloguers freqüents
             bonificacions ++;
 
             // afegeix bonificació per dos dies de lloguer de Luxe
@@ -87,5 +69,27 @@ public class Client {
         resultat += "Import a pagar: " + total + "€\n" +
             "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+  //___________________Refracción_______________________
+    public double quantitatPerLloguer(Lloguer lloguer) {
+    	double quantitat = 0;
+    	switch (lloguer.getVehicle().getCategoria()) {
+        case Vehicle.BASIC:
+            quantitat += 3;
+            if (lloguer.getDies() > 3) {
+                quantitat += (lloguer.getDies() - 3) * 1.5;
+            }
+            break;
+        case Vehicle.GENERAL:
+            quantitat += 4;
+            if (lloguer.getDies() > 2) {
+                quantitat += (lloguer.getDies() - 2) * 2.5;
+            }
+            break;
+        case Vehicle.LUXE:
+            quantitat += lloguer.getDies() * 6;
+            break;
+    	}
+    	return quantitat;
     }
 }
